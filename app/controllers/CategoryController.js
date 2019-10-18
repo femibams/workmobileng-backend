18d5067c43501797b396f9bd73c11185279c6d07
Oluwafemi Bamidele <oluwafemibamidele@Oluwafemis-MacBook-Pro.local>
2019-10-18 16:44:09 +0100

    Create category endpoint completed

--- app/controllers/CategoryController.js
+++ app/controllers/CategoryController.js
@@ -1,8 +1,41 @@
+/**
+ * Created by Fembams on 14/10/19
+ */
+const Response = require("../lib/response_manager");
+const HttpStatus = require("../constants/httpStatus");
+
 class CategoryController {
     
     constructor(logger, categoryService) {
         this.logger = logger;
         this.service = categoryService;
+    }
+
+    /**
+     * 
+     */
+    create(req, res) {
+        const { name, desc } = req.body;
+
+        if(!name)
+            return Response.failure(res, { message: 'Name field is required'}, HttpStatus.BadRequest)
+
+        const categoryData = { name, desc }
+
+        return this.service.create(categoryData)
+            .then(() => {
+                return Response.success(res, {
+                    message: 'Category created successfully'
+                }, HttpStatus.CREATED)
+            })
+            .catch((error) => {
+                this.logger.error(`An error occured whle tryng to save category data -- ${error}`);
+
+                return Response.failure(res, {
+                    message: 'Something went wrong',
+                    response: 'Category data did not save'
+                }, HttpStatus.INTERNAL_SERVER_ERROR);
+            });
     }
 }
 
--- app/routes/category.js
+++ app/routes/category.js
@@ -4,4 +4,9 @@
 
 module.exports.setup = (server, serviceLocator) => {
     const categoryController = serviceLocator.get('categoryController');
+
+    server.get({
+        name: 'Create a category',
+        path: '/category/create'
+    }, (req, res) => categoryController.create(req, res));
 }
